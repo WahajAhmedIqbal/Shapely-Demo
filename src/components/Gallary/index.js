3a699e5ce9d90b942a3031f6c12c52927feeb0eb
WahajAhmedIqbal <wahaj465@gmail.com>
2021-06-22 19:19:12 +0500

    on gallary

--- src/components/Gallary/index.js
+++ src/components/Gallary/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 import { Row, Col, Image } from 'react-bootstrap';
-import holder from '@asset/2.webp'
+import  proImages from '@asset/pro1.jpg'
+import  proImages1 from '@asset/pro2.jpeg'
+import  proImages2 from '@asset/pro3.jpg'
+
 
 import './gallery.css'
 
@@ -11,8 +14,17 @@ const Gallery = () => {
         <div>
             <h3>Our latest Project</h3>
             <h4>Our latest Project, you love them</h4>
-            <Row>
+            <Row >
                 <Col>
+                    <img src={proImages} alt='project1'  />
+                </Col>
+                <Col>
+                    <img src={proImages1} alt='project1' />
+                </Col>
+                <Col>
+                    <img src={proImages2} alt='project1' />
+                </Col>
+                {/* <Col>
                     <h2>
                         this is paragraph i need to check
                     </h2>
@@ -41,8 +53,27 @@ const Gallery = () => {
                     <h2>
                         this is paragraph i need to check
                     </h2>
-                </Col>
+                </Col> */}
             </Row>
+            <h2>
+                test`
+            </h2>
+            <h2>
+                test`
+            </h2>
+            <h2>
+                test`
+            </h2>
+
+            <h2>
+                test`
+            </h2>
+            <h2>
+                test`
+            </h2>
+            <h2>
+                test`
+            </h2>
         </div>
 );
 }
